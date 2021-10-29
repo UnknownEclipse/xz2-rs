7a053afa1e4b38fbeef9b7ed950f49ac74d8757f
ci <ci>
2021-10-29 14:13:20 +0000

    init

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["xz2"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"xz2/stream/enum.Error.html\" title=\"enum xz2::stream::Error\">Error</a>","synthetic":false,"types":["xz2::stream::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
